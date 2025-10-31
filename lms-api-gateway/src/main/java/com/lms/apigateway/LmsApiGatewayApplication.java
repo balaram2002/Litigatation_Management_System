511a99b53d10b0764c900426032cfc8164fa3bbb
Balaram Gochhayat <balaramgochhayat2002@gmail.com>
2025-11-01 00:01:26 +0530

    Initial commit by Balaram Gochhayat

--- lms-api-gateway/src/main/java/com/lms/apigateway/LmsApiGatewayApplication.java
+++ lms-api-gateway/src/main/java/com/lms/apigateway/LmsApiGatewayApplication.java
@@ -1,5 +1,5 @@
 package com.lms.apigateway;
-
+//api gateway
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
